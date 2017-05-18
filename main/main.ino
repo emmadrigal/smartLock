392061eb257afa9491b239f838c3628577f60b0e
Emmanuel Madrigal <emma.madri97@gmail.com>
2017-05-18 10:42:13 -0600

    Finished WiFi connection
    Added Parsing for the recieved messages
    Restrucured WiFi connection code
    Commented Code

--- main/main.ino
+++ main/main.ino
@@ -1,107 +1,135 @@
-/*
-   El ESP8266 debe estar pre-configurado a 115200 Baudios
-   Conectar el ESP8266 de acuerdo al diagrama UNO_ESP_JSON
-*/
-
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(3, 2); //Pines: RX conectado a D3, TX conectado a D2
+//Pines: RX conectado a D3, TX conectado a D2
+SoftwareSerial mySerial(3, 2);
 
-//
-#define DEBUG_ESP8266 //Comentar si no se quiere imprimir la respuesta del ESP8266
-#define ESP8266_OK //Confirmar si el comando AT fue recibido
+#define SSID   "smartLockAP"
+#define PASS   "pwd"
 
-#define SSID   "FMC_Sala"
-#define PASS   "25912496"
+//Guarda el mensaje recibido por puerto serial
+String recibido;
+
 
 //****************************************** FUNCIONES****************************************************************//
-
-void connectToWiFi() {       // Función que permite conectarse al servidor específicado en el string server
-  SendCmd("ATE1", 60);//Disables echo
+/**
+ * Función que permite conectarse al servidor específicado en el string server
+ */
+void connectToWiFi() {
+  //Disables echo
+  SendCmd("ATE1", 5);
   
-  //SendCmd("AT+GSLP=1500", 60);//Enter Deepsleep for 1.5s
+  //SendCmd("AT+GSLP=1500", 5);//Enter Deepsleep for 1.5s
 
+/*
   //Begins client Mode
-  SendCmd("AT+CWMODE=1", 60);
+  SendCmd("AT+CWMODE=1", 5);
   
   // Comando AT para la conexión WiFi seleccionada con el SSID y PASS
   String AP = "AT+CWJAP=\""; AP += SSID; AP += "\",\""; AP += PASS; AP += "\"";
-  SendCmd(AP, 60);
+  SendCmd(AP, 5);
+*/
+  //Starts its own AP
+  SendCmd("AT+CWMODE=2", 5);
 
+  //Configures its own AP
+  String AP = "AT+CWSAP=\"";
+  AP += SSID;
+  AP += "\",\"";
+  AP += PASS;
+  AP += "\",5,0,4";
+  
+  SendCmd(AP, 5);
+  
   //Enables up to 4 connections
-  SendCmd("AT+CIPMUX=1", 60);
+  SendCmd("AT+CIPMUX=1", 5);
 
   //Start Server on port 333
-  SendCmd("AT+CIPSERVER=1", 60);
+  SendCmd("AT+CIPSERVER=1", 5);
 }
 
-void resetESP() {
-  Serial.println("RST");
-  mySerial.println("AT+RST");        // Deshabilita el echo de los comandos enviados
-  delay(20);
-  connectToWiFi();                 // Función para conectar el módulo ESP8266 a la red WiFi selecccionada
+/**
+ * Obtiene los datos del módulo WiFi y ejecuta la instrucción correcta
+ */
+void parseData(){
+  //Abre la puerta
+  if(recibido.charAt(0) == '0'){
+    Serial.println("abrir la puerta");
+  }
+  //Cierra la puerta
+  else if(recibido.charAt(0) == '1'){
+    Serial.println("cerrar la puerta");
+  }
+  //Agrega una nueva tarjeta a los posibles registros
+  else if(recibido.charAt(0) == '2'){
+    Serial.print("Agregar la tarjeta: ");
+    Serial.println(recibido.substring(2));
+  }
+  //Elimina una tarjeta de los posibles registros
+  else if(recibido.charAt(0) == '3'){
+    Serial.print("Eliminar la tarjeta: ");
+    Serial.println(recibido.substring(2));
+  }
+  //Pregunta por el status
+  else if(recibido.charAt(0) == '4'){
+    Serial.println("Status?");
+  }
+  //Mensaje desconocido
+  else{
+    Serial.println("Unknown status");
+  }
 }
 
+/**
+ * Lee datos del módulo WiFi
+ */
 void leer() {
-  while (mySerial.available()) {
-    String recibido = "";
-    if (mySerial.available()) {
-      recibido += (char)mySerial.read();
-    }
-    Serial.print(recibido);
+  //Si hay datos para leer
+  if (mySerial.available()) {
+    //Lee todos los datos disponibles
+    recibido = mySerial.readString();
+
+    //Busca el inicio del mensaje en sí (el módulo escribe "+IDP, 0,1:msj")
+    int index = recibido.indexOf(':') + 1;
+    recibido = recibido.substring(index);
+
+    //Utiliza los datos
+    parseData();
   }
 }
 
-bool findOK() {                     //Función que permite verificar el resultado "OK" del comando AT
-  if (mySerial.find("OK"))         // Si se localiza OK en la respuesta del ESP8266
-  {
-    Serial.println("OK");
-    return true;                    // Devuelve "True"
-  }
-  else
-  {
-    //Serial.println("!OK");
-    return false;                   // Retorna "False"
-  }
-}
-
+/**
+ * Envia un comando al módulo WiFi
+ * @param Atcmd comando AT a enviar al módulo WiFi
+ * @param Tespera tiempo a esperar luego de enviar el comando
+ */
 void SendCmd (String ATcmd, int Tespera) {
-
-#ifdef DEBUG_ESP8266
+  //Envia el dato al ESP8266
   mySerial.println(ATcmd);
-  delay(10); //Tiempo a esperar para abrir el puerto mySerial
-  leer();
-#endif
-
-#ifdef ESP8266_OK
-  while (!findOK()) {
-    mySerial.println(ATcmd);
-    delay(Tespera); //Tiempo a esperar para abrir el puerto mySerial
-  }
-#endif
-  delay(60);
+  //Espera Tespera
+  delay(Tespera);
 }
-
-//----------------------------------------------------------------------------------------------------------------
-
-bool askApprovedID(){
-
-  return true;
-}
-
-
 
 //-----------------------------------------------------------------------------------------------------------------
+/**
+ * Inicializa el arduino
+ */
 void setup() {
+  // Inicializacion del Monitor Serial a 115200 para Debugeo
+  Serial.begin(38400);
+  // Inicializacion de un puerto serial para conectarse a Wifi
+  mySerial.begin(38400);
+  //Pone el máximo tiempo de espera a 50ms
+  mySerial.setTimeout(50);
 
-  Serial.begin(38400);             // Inicializacion del Monitor Serial a 115200 para Debugeo
-  mySerial.begin(38400);           // Inicializacion de un puerto serial para conectarse a Wifi
-
-  connectToWiFi();                 // Función para conectar el módulo ESP8266 a la red WiFi selecccionada
+  // Función para crear un AP con el módulo ESP8266
+  connectToWiFi();
   delay(20);
 }
 
+/**
+ * Inicializa el loop de control
+ */
 void loop() {
-  //Chequea si hay datos a leer
+  //Chequea si hay datos a leer del módulo WiFi
   leer();
 }
--- pythonServerTest/serverTest.py
+++ pythonServerTest/serverTest.py
@@ -5,10 +5,10 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.1.11', 333)
+server_address = ('192.168.4.1', 333)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
-sock.send("Hi")
-
-sock.send("I'm your father")
+while(True):
+    data = input("write a message: \n")
+    sock.send(data)
